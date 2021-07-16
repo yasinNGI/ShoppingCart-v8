c2d65b1a8f220cb06a453d1773cd959db59c80f5
yasinNGI <yasin@nextgeni.com>
2021-07-16 16:23:29 +0500

    Code updated - Cart Module
    Signed-off-by: yasinNGI <yasin@nextgeni.com>

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,19 @@
+<!-- Page Header -->
+<nav class="bg-white border-b border-gray-100 custom-navbar">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-3 py-1">
+                <a class="navbar-brand" href="{{$homeUrl}}">
+                    {{ $siteName }}
+                </a>
+            </div>
+            <div class="col-lg-9 py-2 text-right">
+                <a href="{{$cartUrl}}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                    Cart <span id="cart_item">({{$cartCounter}})</span>
+                </a>
+            </div>
+        </div>
+    </div>
+
+</nav>
