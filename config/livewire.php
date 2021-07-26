74c2343f2f08f325081b09a508546e27e8768288
yasinNGI <yasin@nextgeni.com>
2021-07-26 10:31:01 +0500

    Code updated with live-wire still working
    Signed-off-by: yasinNGI <yasin@nextgeni.com>

--- config/livewire.php
+++ config/livewire.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'asset_url' => null,
+    'asset_url' => 'http://shoppingcart-v8.herokuapp.com',
 
     /*
     |--------------------------------------------------------------------------
